09cb52dd93ae43ed30541cf254b40bcc4080558d
kongfanjie <2112975468@qq.com>
2026-07-09 22:48:44 +0800

    Create rules.py

---
+++ 02_course_materials_organizer/course_organizer/rules.py
@@ -0,0 +1,26 @@
+HOMEWORK_KEYWORDS = ["作业", "练习", "实验", "任务"]
+
+EXTENSION_RULES = {
+    ".ppt": "slides",
+    ".pptx": "slides",
+    ".key": "slides",
+    ".py": "code",
+    ".ipynb": "code",
+    ".c": "code",
+    ".cpp": "code",
+    ".java": "code",
+    ".csv": "data",
+    ".xlsx": "data",
+    ".json": "data",
+    ".pdf": "documents",
+    ".doc": "documents",
+    ".docx": "documents",
+    ".txt": "documents",
+    ".md": "documents",
+    ".png": "images",
+    ".jpg": "images",
+    ".jpeg": "images",
+    ".gif": "images",
+}
+
+DEFAULT_CATEGORY = "others"
